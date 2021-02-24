491339cfc7df2dc0ea5092acb6e84d43fe5a3497
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2021-02-24 09:46:32 -0600

    Create index.ts

---
+++ 9/index.ts
@@ -0,0 +1,12 @@
+const isPalindrome = (x: number): boolean => {
+    if (x < 0) return false
+
+    let reversed = 0, y = x
+
+    while (y > 0) {
+        const lastDigit = y % 10
+        reversed = (reversed * 10) + lastDigit
+        y = (y / 10) | 0
+    }
+    return x === reversed
+}
